95d0177de4ec905fff00c1cf302992767a957cc9
Jhonel Villanueva <vvjhonel@gmail.com>
2022-12-03 02:22:08 +0800

    Add formatting function for user's input

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ const getComputerChoice = () => {
       return "Scissors";
   }
 };
+
+// capitalize(string) will format user's choice to capital first letter only
+
+const capitalize = (string) => {
+  let lowerCase = string.toLowerCase();
+  return lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+};
